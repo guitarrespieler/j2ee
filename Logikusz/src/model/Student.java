18665ee8eff98bbef3b68792b2e3a5d6db10fce3
Zsiga Tibor <tibor.zsiga@hotmail.com>
2018-05-05 20:46:11 +0200

    saved...

--- Logikusz/src/model/Student.java
+++ Logikusz/src/model/Student.java
@@ -23,4 +23,72 @@ public class Student implements Serializable{
 	@JoinColumn(name="group_id")
 	private Group group;
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + birthYear;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + studentId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (birthYear != other.birthYear)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (studentId != other.studentId)
+			return false;
+		return true;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	public Group getGroup() {
+		return group;
+	}
+
+	public void setGroup(Group group) {
+		this.group = group;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	
 }
